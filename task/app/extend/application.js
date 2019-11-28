7e45213c5bb1f35b1734788a4d15d4c8a441352c
啊平 <951984189@qq.com>
2019-11-28 10:50:20 +0800

    修复windows底下获得文件名错误

--- task/app/extend/application.js
+++ task/app/extend/application.js
@@ -1,7 +1,12 @@
 'use strict';
 
+const os = require('os');
+
 module.exports = {
     fileName(filePathName) {
+        if (os.type().toLowerCase().includes('windows')) {
+            return filePathName.substring(filePathName.lastIndexOf('\\') + 1, filePathName.lastIndexOf('.'))
+        }
         return filePathName.substring(filePathName.lastIndexOf('/') + 1, filePathName.lastIndexOf('.'))
     }
 };
